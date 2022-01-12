f71d185c0178fdbf1f8953d1fdfa4a441118f761
ApoloRezende <apolo_henriquerr@hotmail.com>
2022-01-12 00:01:51 -0300

    novo pull request

---
+++ script.js
@@ -0,0 +1,15 @@
+const buttonLogin = document.getElementById('entrar');
+const loginAdress = document.getElementById('login-email');
+const password = document.getElementById('login-senha');
+const buttonSubmit = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
+
+function alerta() {
+  if (loginAdress.value === 'tryber@teste.com' && password.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+}
+
+buttonLogin.addEventListener('click', alerta);
