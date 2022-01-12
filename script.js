a6e7f9d8cd656f8588c5a860d305261b5ab83201
ApoloRezende <apolo_henriquerr@hotmail.com>
2022-01-12 00:49:19 -0300

    req 18 e 19 ok

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttonLogin = document.getElementById('entrar');
 const loginAdress = document.getElementById('login-email');
 const password = document.getElementById('login-senha');
+const buttonSubmit = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
 
 function alerta() {
   if (loginAdress.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +13,10 @@ function alerta() {
 }
 
 buttonLogin.addEventListener('click', alerta);
+
+function enableButton() {
+  if (agree.checked === true) {
+    buttonSubmit.removeAttribute('disabled');
+  }
+}
+agree.addEventListener('input', enableButton);
